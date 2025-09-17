91f170f5f4b456e7fbfde822b40e1e15f30867bf
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2025-09-17 22:30:20 +0000

    criado packages.yml e modelo staging

---
+++ models/staging/erp/stg_erp__sales_customer.sql
@@ -0,0 +1,24 @@
+with
+    source_customer as (
+        select *
+        from {{ source('erp', 'sales_customer') }}
+    )
+
+    , renamed as (
+        select
+            cast(customerid as int) as customer_pk
+            , cast(personid as int) as person_fk
+            , cast(storeid as int) as store_fk
+            , cast(territoryid as int) as territory_fk
+            , cast(rowguid as string) as customer_row_guid
+            , cast(modifieddate as timestamp) as customer_modified_date
+            , case 
+                when person_fk is not null then 'Individual'
+                when store_fk is not null then 'Store'
+                else 'Unknown'
+            end as customer_type
+        from source_customer
+    )
+    
+select *
+from renamed
